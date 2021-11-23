1d589f2a8560c9177f4108f94b1a4bd1618ad92c
Michel <mnemonic01@users.noreply.github.com>
2021-11-23 16:19:42 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "tenancy_ocid" {}
+variable "user_ocid" {}
+variable "fingerprint" {}
+variable "private_key_path" {}
+variable "region" {}
+variable "vcn_cidr" {
+  default = "10.0.0.0/16"
+}
