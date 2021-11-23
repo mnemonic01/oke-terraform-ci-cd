d8159fa63543b96dfe4618f2b1331bd98b9fc368
Michel <mnemonic01@users.noreply.github.com>
2021-11-23 15:37:59 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,15 +1,6 @@
-# provider "oci" {
- # region = var.region
-#  tenancy_ocid = var.tenancy_ocid
- # user_ocid = var.user_ocid
-#  fingerprint = var.fingerprint
-#  private_key_path = var.private_key_path
-
-provider "oci" {
-  alias            = "home"
-  region           = "${lookup(data.oci_identity_regions.home-region.regions[0], "name")}"
-  tenancy_ocid     = "${var.tenancy_ocid}"
-  user_ocid        = "${var.user_ocid}"
-  fingerprint      = "${var.fingerprint}"
-  private_key_path = "${var.private_key_path}"
-}
+ provider "oci" {
+ region = var.region
+ tenancy_ocid = var.tenancy_ocid
+ user_ocid = var.user_ocid
+ fingerprint = var.fingerprint
+ private_key_path = var.private_key_path
