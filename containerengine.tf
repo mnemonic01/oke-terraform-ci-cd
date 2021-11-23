6744865d9fa860310845e35a9792339fa5a9da06
Michel <mnemonic01@users.noreply.github.com>
2021-11-23 17:07:24 +0100

    Update containerengine.tf

--- containerengine.tf
+++ containerengine.tf
@@ -28,7 +28,8 @@ resource oci_containerengine_node_pool pool1 {
   #kubernetes_version = data.oci_containerengine_node_pool_option.pool1_option.kubernetes_versions.2
   kubernetes_version = "v1.19.7"
   name               = "pool1"
-  node_shape = "VM.Standard1.2"
+  node_image     = "Oracle-Linux-7.5"
+  node_shape     = "VM.Standard2.2"
 
   node_config_details {
     placement_configs {
@@ -47,8 +48,8 @@ resource oci_containerengine_node_pool pool1 {
   }
   
   node_source_details {
-    node_image     = "Oracle-Linux-7.5"
-    node_shape     = "VM.Standard2.2"
+    #node_image     = "Oracle-Linux-7.5"
+    #node_shape     = "VM.Standard2.2"
     #image_id    = (var.node_pool_image_id == "none" && length(regexall("GPU|A1", lookup(each.value, "shape"))) == 0) ? (element([for source in local.node_pool_image_ids : source.image_id if length(regexall("Oracle-Linux-${var.node_pool_os_version}-20[0-9]*.*", source.source_name)) > 0], 0)) : (var.node_pool_image_id == "none" && length(regexall("GPU", lookup(each.value, "shape"))) > 0) ? (element([for source in local.node_pool_image_ids : source.image_id if length(regexall("Oracle-Linux-${var.node_pool_os_version}-Gen[0-9]-GPU-20[0-9]*.*", source.source_name)) > 0], 0)) : (var.node_pool_image_id == "none" && length(regexall("A1", lookup(each.value, "shape"))) > 0) ? (element([for source in local.node_pool_image_ids : source.image_id if length(regexall("Oracle-Linux-${var.node_pool_os_version}-aarch64-20[0-9]*.*", source.source_name)) > 0], 0)) : var.node_pool_image_id
     #source_type = data.oci_containerengine_node_pool_option.node_pool_options.sources[0].source_type
     boot_volume_size_in_gbs = 60
