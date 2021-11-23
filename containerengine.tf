54ea364ea733e67068018f6c0151252f4bcfe369
Michel <mnemonic01@users.noreply.github.com>
2021-11-23 17:13:13 +0100

    Update containerengine.tf

--- containerengine.tf
+++ containerengine.tf
@@ -49,8 +49,8 @@ resource oci_containerengine_node_pool pool1 {
   node_source_details {
     #node_image     = "Oracle-Linux-7.5"
     #node_shape     = "VM.Standard2.2"
-    #image_id    = (var.node_pool_image_id == "none" && length(regexall("GPU|A1", lookup(each.value, "shape"))) == 0) ? (element([for source in local.node_pool_image_ids : source.image_id if length(regexall("Oracle-Linux-${var.node_pool_os_version}-20[0-9]*.*", source.source_name)) > 0], 0)) : (var.node_pool_image_id == "none" && length(regexall("GPU", lookup(each.value, "shape"))) > 0) ? (element([for source in local.node_pool_image_ids : source.image_id if length(regexall("Oracle-Linux-${var.node_pool_os_version}-Gen[0-9]-GPU-20[0-9]*.*", source.source_name)) > 0], 0)) : (var.node_pool_image_id == "none" && length(regexall("A1", lookup(each.value, "shape"))) > 0) ? (element([for source in local.node_pool_image_ids : source.image_id if length(regexall("Oracle-Linux-${var.node_pool_os_version}-aarch64-20[0-9]*.*", source.source_name)) > 0], 0)) : var.node_pool_image_id
-    #source_type = data.oci_containerengine_node_pool_option.node_pool_options.sources[0].source_type
+    image_id    = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaajlbjf3l5bthwf6nphctu33ytm65r237bmbdjiik4jyfdxjx5iiea"
+    source_type = "image"
     boot_volume_size_in_gbs = 60
   }
   #ssh_public_key      = var.node_pool_ssh_public_key
